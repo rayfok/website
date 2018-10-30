e98ac2cbcb393535cac97f9a6ad97468f01d6f8e
eungkeun <eungkeun@umich.edu>
2018-10-30 01:32:30 -0400

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteMetadata: {
+    title: "Raymond Fok",
+    author: "Raymond Fok",
+    description: "A Glimpse Into The Life of Researcher"
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline'
+  ],
+  pathPrefix: "/rayfok_website",
+}
---
+++ src/components/Main.js
@@ -0,0 +1,107 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import ASSETS2018_SpeechAccess from '../pdfs/ASSETS2018_SpeechAccess.pdf'
+import CI2017_TowardsHybridIntelligenceForRobotics_POSTER from '../pdfs/CI2017_TowardsHybridIntelligenceForRobotics_POSTER.pdf'
+import CI2017_TowardsHybridIntelligenceForRobotics from '../pdfs/CI2017_TowardsHybridIntelligenceForRobotics.pdf'
+import GroupSight2017_FourEyes from '../pdfs/GroupSight2017_FourEyes.pdf'
+import IUI2018_FourEyes from '../pdfs/IUI2018_FourEyes.pdf'
+import TiiS2018_FourEyes from '../pdfs/TiiS2018_FourEyes.pdf'
+import UIST2017_WearMail from '../pdfs/UIST2017_WearMail.pdf'
+
+class Main extends React.Component {
+  render() {
+
+    let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
+
+    return (
+      <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
+
+        <article id="research" className={`${this.props.article === 'research' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">Research</h2>
+          <p>I'm a senior at the University of Michigan studying computer science and math, and a member of the Crowds and Machines Lab, advised by <a href="https://web.eecs.umich.edu/~wlasecki/" target="_blank" rel="noopener noreferrer">Dr. Walter S. Lasecki</a>. My research interests lie in the intersection of human-computer interaction and artificial intelligence, focusing on the design and development of hybrid intelligent systems for a diverse range of applications.</p>
+
+          <p>I'm currently working on several research projects, from exploring how we can use crowdsourced feedback to improve training of reinforcement learning agents in OpenAI Gym, to integrating human intelligence in the form of semantic understanding for realtime, adaptive time-series modeling. Stay tuned for these exciting papers in the coming months.
+          </p>
+
+          <h3>
+            Publications
+          </h3>
+
+          <div class='publication'>
+            <b>R. Fok</b>, H. Kaur, S. Palani, M.E. Mott, W.S. Lasecki. <a href={ASSETS2018_SpeechAccess} target="_blank" rel="noopener noreferrer">Towards More Robust Speech Interactions for Deaf and Hard of Hearing Users</a>. In <i>Proceedings of the International ACM SIGACCESS Conference on Computers and Accessibility (ASSETS 2018)</i>. Galway, Ireland.
+          </div>
+
+          <div class='publication'>
+            J.Y. Song, <b>R. Fok</b>, J. Kim, W.S. Lasecki. <a href={TiiS2018_FourEyes} target="_blank" rel="noopener noreferrer">FourEyes: Leveraging Tool Diversity as a Means to Improve Aggregate Accuracy in Crowdsourcing.</a> In <i>ACM Transactions on Interactive Intelligent Systems (TiiS)</i>. 2018.
+          </div>
+
+          <div class='publication'>
+            J.Y. Song, <b>R. Fok</b>, A. Lundgard, F. Yang, J. Kim, W.S. Lasecki. <a href={IUI2018_FourEyes} target="_blank" rel="noopener noreferrer">Two Tools are Better Than One: Tool Diversity as a Means of Improving Aggregate Crowd Performance</a>. In <i>Proceedings of the ACM International Conference on Intelligent User Interfaces (IUI 2018)</i>. Tokyo, Japan. <span class='paperAward'>Best Student Paper Honorable Mention</span>
+          </div>
+
+          <div class='publication'>
+            S. Swaminathan, <b>R. Fok</b>, F. Chen, T.K. Huang, I. Lin, R. Jadvani, W.S. Lasecki, J.P. Bigham. <a href={UIST2017_WearMail} target="_blank" rel="noopener noreferrer">WearMail: On-the-Go Access to Information in Your Email with a Privacy-Preserving Human Computation Workflow</a>. In <i>Proceedings of the ACM Symposium on User Interface Software and Technology (UIST 2017)</i>. Quebec City, Canada.
+          </div>
+
+          <div class='publication'>
+            J.Y. Song, <b>R. Fok</b>, F. Yang, K. Wang, A.R. Lundgard, W.S. Lasecki. <a href={GroupSight2017_FourEyes} target="_blank" rel="noopener noreferrer">Tool Diversity as a Means of Improving Aggregate Crowd Performance on Image Segmentation Tasks</a>. In <i>HCOMP Workshop on Human Computation for Image and Video Analysis (GroupSight 2017)</i>. Quebec City, Quebec. 2017.
+          </div>
+
+          <div class='publication'>
+            S.R. Gouravajhala, J.Y. Song, J. Yim, <b>R. Fok</b>, Y. Huang, F. Yang, K. Wang, Y. An, W.S. Lasecki. <a href={CI2017_TowardsHybridIntelligenceForRobotics} target="_blank" rel="noopener noreferrer">Towards Hybrid Intelligence for Robotics</a>. In <i>Collective Intelligence Conference (CI 2017)</i>. New York, NY. [<a href={CI2017_TowardsHybridIntelligenceForRobotics_POSTER} target="_blank" rel="noopener noreferrer">POSTER</a>]
+          </div>
+          {close}
+        </article>
+
+        <article id="blog" className={`${this.props.article === 'blog' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">Blog</h2>
+          <p>Coming soon!</p>
+          {close}
+        </article>
+
+        <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">Contact</h2>
+          <form method="post" action="#">
+            <div className="field half first">
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" id="name" />
+            </div>
+            <div className="field half">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" id="email" />
+            </div>
+            <div className="field">
+              <label htmlFor="message">Message</label>
+              <textarea name="message" id="message" rows="4"></textarea>
+            </div>
+            <ul className="actions">
+              <li><input type="submit" value="Send Message" className="special" /></li>
+              <li><input type="reset" value="Reset" /></li>
+            </ul>
+          </form>
+          <ul className="icons">
+            <li><a href="https://twitter.com/" className="icon fa-twitter" target="_blank" rel="noopener noreferrer"><span className="label">Twitter</span></a></li>
+            <li><a href="https://www.facebook.com/raymond.fok.75" className="icon fa-facebook" target="_blank" rel="noopener noreferrer"><span className="label">Facebook</span></a></li>
+            <li><a href="https://github.com/rayfok" className="icon fa-github" target="_blank" rel="noopener noreferrer"><span className="label" >GitHub</span></a></li>
+            <li><a href="https://www.linkedin.com/in/raymond-fok/" className="icon fa-linkedin" target="_blank" rel="noopener noreferrer"><span className="label">LinkedIn</span></a></li>
+            <li><a href="https://scholar.google.com/citations?user=j238omAAAAAJ" className="icon fa-google" target="_blank" rel="noopener noreferrer"><span className="label">Google Scholar</span></a></li>
+          </ul>
+          {close}
+        </article>
+
+      </div>
+    )
+  }
+}
+
+Main.propTypes = {
+  route: PropTypes.object,
+  article: PropTypes.string,
+  articleTimeout: PropTypes.bool,
+  onCloseArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+  setWrapperRef: PropTypes.func.isRequired,
+}
+
+export default Main
